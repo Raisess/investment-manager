69ee4a4374b8e0c163b0161868e97e06defea4a9
Raisess <raisechif@gmail.com>
2026-07-02 11:41:05 -0300

    fix: rentability ids

--- __migrations/1742393940_investment_rentability_table.sql
+++ __migrations/1742393940_investment_rentability_table.sql
@@ -7,8 +7,8 @@ CREATE TABLE IF NOT EXISTS investment_rentabilities(
 
 INSERT INTO investment_rentabilities(id, name) VALUES
   ('bed67de6-a427-4d42-8970-85ccfc7da932', 'IPCA'),
-  ('b7ec23d6-1318-4510-a884-984f866ea2f1', 'IPCA + %% a.a'),
+  ('22101414-68b6-4d23-a8bb-7b9c72f44843', 'IPCA + %% a.a'),
   ('d95b6f39-8c8a-4ab7-af58-4b742553971b', 'SELIC'),
-  ('4eae2572-2741-47db-89ed-17e81cc2286a', 'SELIC + %% a.a'),
+  ('5d87ade2-2cd6-476d-aaeb-0c51eb58a20a', 'SELIC + %% a.a'),
   ('37d9a84f-7c64-4568-b87e-7c1b4a7c28fd', '%% a.a'),
-  ('8eccbbb1-004b-4953-a516-31f8264dd8ab', 'CDI');
+  ('741de8f4-0d2e-44cb-a445-bdf931d993fc', 'CDI');
